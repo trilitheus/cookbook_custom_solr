28b9a80fb69b4d93125fa3957e0d7dad340410dc
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-08-30 16:13:09 +0100

    Add attributes for solr version/url

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,7 @@
+solr_version = '5.5.2'
+
 default['custom_solr']['java_xmx'] = (node['memory']['total'].to_i * 3 / 8).floor
 default['custom_solr']['java_xms'] = (node['memory']['total'].to_i * 3 / 8).floor
+
+default['custom_solr']['version'] = solr_version
+default['custom_solr']['url'] = "http://archive.apache.org/dist/lucene/solr/#{solr_version}"
