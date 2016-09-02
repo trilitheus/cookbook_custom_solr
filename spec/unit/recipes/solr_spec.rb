bc6571ff45f2cb622287ca414a060d2bc9b2a281
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-09-02 14:01:20 +0100

    Remove useless stub and add variable for version

--- spec/unit/recipes/solr_spec.rb
+++ spec/unit/recipes/solr_spec.rb
@@ -6,7 +6,7 @@
 
 require 'spec_helper'
 
-describe 'custom_solr::default' do
+describe 'custom_solr::solr' do
   context 'When all attributes are default, on RHEL7' do
     cached(:chef_run) do
       ChefSpec::ServerRunner.new(file_cache_path: '/tmp') do |node|
@@ -14,17 +14,7 @@ describe 'custom_solr::default' do
       end.converge(described_recipe)
     end
 
-    before do
-      File.stub(:uid).with('/opt/solr/bin/solr').and_return(6001)
-    end
-
-    it 'includes the java recipe' do
-      expect(chef_run).to include_recipe('alliantist_solr::java')
-    end
-
-    it 'includes the monit-ng cookbook' do
-      expect(chef_run).to include_recipe('monit-ng::default')
-    end
+    solr_version = '5.5.2'
 
     it 'creates the solr group' do
       expect(chef_run).to create_group('solr').with_gid(6001)
@@ -39,15 +29,15 @@ describe 'custom_solr::default' do
     end
 
     it 'downloads the solr tgz file' do
-      expect(chef_run).to create_remote_file_if_missing('/tmp/solr-5.5.1.tgz')
+      expect(chef_run).to create_remote_file_if_missing("/tmp/solr-#{solr_version}.tgz")
     end
 
     it 'unpacks the solr_archive' do
-      expect(chef_run).to run_bash('tar xzf /tmp/solr-5.5.1.tgz').with_cwd('/opt')
+      expect(chef_run).to run_bash("tar xzf /tmp/solr-#{solr_version}.tgz").with_cwd('/opt')
     end
 
     it 'links the solr versioned dir to non versioned' do
-      expect(chef_run).to create_link('/opt/solr').with(to: '/opt/solr-5.5.1')
+      expect(chef_run).to create_link('/opt/solr').with(to: "/opt/solr-#{solr_version}")
     end
 
     it 'adds the collection1 tar file' do
@@ -64,7 +54,7 @@ describe 'custom_solr::default' do
     end
 
     it 'changes ownership of the solr directory' do
-      expect(chef_run).to run_bash('chown -R solr:solr /opt/solr-5.5.1')
+      expect(chef_run).to run_bash("chown -R solr:solr /opt/solr-#{solr_version}")
     end
 
     it 'adds monit service for solr' do
